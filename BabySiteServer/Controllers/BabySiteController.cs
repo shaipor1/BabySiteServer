8f5f231a9b14d931f9733ab9b8b0878a593df32c
User <User@LAB2-11>
2021-10-10 09:15:13 +0300

    Add project files.

---
+++ BabySiteServer/Controllers/BabySiteController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+//Add the below
+using BabySiteServerBL.Models;
+using System.IO;
+
+namespace BabySiteServer.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BabySiteController : ControllerBase
+    {
+        [Route("BabySiteAPI")]
+        [ApiController]
+        public class ContactsController : ControllerBase
+        {
+            #region Add connection to the db context using dependency injection
+            BabySiteDBContext context;
+            public ContactsController(BabySiteDBContext context)
+            {
+                this.context = context;
+            }
+            #endregion
+        }
+    }
+}
