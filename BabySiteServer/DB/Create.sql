578f26bc5d73bff46d913a8c3c329dc232b8994c
User <User@LAB2-11>
2021-10-13 13:33:08 +0300

    co

---
+++ BabySiteServer/DB/Create.sql
@@ -0,0 +1,134 @@
+CREATE DATABASE "BabySiteDB"
+GO
+USE "BabySiteDB"
+GO
+CREATE TABLE "User"(
+    "UserId" INT NOT NULL,
+    "UserTypeId" INT NOT NULL,
+    "LocationId" INT NOT NULL,
+    "FirstName" NVARCHAR(255) NOT NULL,
+    "LastName" NVARCHAR(255) NOT NULL,
+    "PhoneNumber" NVARCHAR(255) NOT NULL,
+    "Email" NVARCHAR(255) NOT NULL,
+    "UserName" NVARCHAR(255) NOT NULL,
+    "UserPswd" NVARCHAR(255) NOT NULL
+);
+ALTER TABLE
+    "User" ADD CONSTRAINT "user_userid_primary" PRIMARY KEY("UserId");
+CREATE TABLE "UserType"(
+    "UserTypeId" INT NOT NULL,
+    "UserTypeName" NVARCHAR(255) NOT NULL
+);
+ALTER TABLE
+    "UserType" ADD CONSTRAINT "usertype_usertypeid_primary" PRIMARY KEY("UserTypeId");
+CREATE TABLE "Massage"(
+    "MassageId" INT NOT NULL,
+    "MassageTypeId" INT NOT NULL,
+    "UserId" INT NOT NULL,
+    "HeadLine" NVARCHAR(255) NOT NULL,
+    "Body" NVARCHAR(255) NOT NULL
+);
+ALTER TABLE
+    "Massage" ADD CONSTRAINT "massage_massageid_primary" PRIMARY KEY("MassageId");
+CREATE TABLE "MassageType"(
+    "MassageTypeId" INT NOT NULL,
+    "UserTypeId" INT NOT NULL,
+    "MassageTypeName" NVARCHAR(255) NOT NULL
+);
+ALTER TABLE
+    "MassageType" ADD CONSTRAINT "massagetype_massagetypeid_primary" PRIMARY KEY("MassageTypeId");
+CREATE TABLE "BabySitter"(
+    "BabySitterId" INT NOT NULL,
+    "UserId" INT NOT NULL,
+    "RatingAverage" INT NOT NULL,
+    "HasCar" BIT NOT NULL,
+    "Age" INT NOT NULL,
+    "Salary" INT NOT NULL
+);
+ALTER TABLE
+    "BabySitter" ADD CONSTRAINT "babysitter_babysitterid_primary" PRIMARY KEY("BabySitterId");
+CREATE TABLE "Parents"(
+    "ParentId" INT NOT NULL,
+    "UserId" INT NOT NULL,
+    "ChildrenCount" INT NOT NULL,
+    "ChildrenMinAge" INT NOT NULL,
+    "ChildrenMaxAge" INT NOT NULL,
+    "HasDog" BIT NOT NULL
+);
+ALTER TABLE
+    "Parents" ADD CONSTRAINT "parents_parentid_primary" PRIMARY KEY("ParentId");
+CREATE TABLE "Location"(
+    "LocationId" INT NOT NULL,
+    "CityId" INT NOT NULL,
+    "HouseId" INT NOT NULL,
+    "Street" NVARCHAR(255) NOT NULL
+);
+ALTER TABLE
+    "Location" ADD CONSTRAINT "location_locationid_primary" PRIMARY KEY("LocationId");
+CREATE TABLE "Request"(
+    "RequestId" INT NOT NULL,
+    "ParentId" INT NOT NULL,
+    "BabySitterId" INT NOT NULL,
+    "MassageId" INT NOT NULL,
+    "RequestStatusId" INT NOT NULL
+);
+ALTER TABLE
+    "Request" ADD CONSTRAINT "request_requestid_primary" PRIMARY KEY("RequestId");
+CREATE TABLE "Reviews"(
+    "ReviewId" INT NOT NULL,
+    "ParentId" INT NOT NULL,
+    "BabySitterId" INT NOT NULL,
+    "Rating" INT NOT NULL,
+    "Decription" NVARCHAR(255) NOT NULL
+);
+ALTER TABLE
+    "Reviews" ADD CONSTRAINT "reviews_reviewid_primary" PRIMARY KEY("ReviewId");
+CREATE TABLE "Area"(
+    "AreaId" INT NOT NULL,
+    "AreaName" INT NOT NULL
+);
+ALTER TABLE
+    "Area" ADD CONSTRAINT "area_areaid_primary" PRIMARY KEY("AreaId");
+CREATE TABLE "City"(
+    "CityId" INT NOT NULL,
+    "CityName" INT NOT NULL,
+    "AreaId" INT NOT NULL
+);
+ALTER TABLE
+    "City" ADD CONSTRAINT "city_cityid_primary" PRIMARY KEY("CityId");
+CREATE TABLE "RequestStatus"(
+    "RequestStatusId" INT NOT NULL,
+    "RequestStatusName" NVARCHAR(255) NOT NULL
+);
+ALTER TABLE
+    "RequestStatus" ADD CONSTRAINT "requeststatus_requeststatusid_primary" PRIMARY KEY("RequestStatusId");
+ALTER TABLE
+    "BabySitter" ADD CONSTRAINT "babysitter_userid_foreign" FOREIGN KEY("UserId") REFERENCES "User"("UserId");
+ALTER TABLE
+    "Massage" ADD CONSTRAINT "massage_userid_foreign" FOREIGN KEY("UserId") REFERENCES "User"("UserId");
+ALTER TABLE
+    "Parents" ADD CONSTRAINT "parents_userid_foreign" FOREIGN KEY("UserId") REFERENCES "User"("UserId");
+ALTER TABLE
+    "User" ADD CONSTRAINT "user_usertypeid_foreign" FOREIGN KEY("UserTypeId") REFERENCES "UserType"("UserTypeId");
+ALTER TABLE
+    "MassageType" ADD CONSTRAINT "massagetype_usertypeid_foreign" FOREIGN KEY("UserTypeId") REFERENCES "UserType"("UserTypeId");
+ALTER TABLE
+    "Request" ADD CONSTRAINT "request_massageid_foreign" FOREIGN KEY("MassageId") REFERENCES "Massage"("MassageId");
+ALTER TABLE
+    "Massage" ADD CONSTRAINT "massage_massagetypeid_foreign" FOREIGN KEY("MassageTypeId") REFERENCES "MassageType"("MassageTypeId");
+ALTER TABLE
+    "Reviews" ADD CONSTRAINT "reviews_babysitterid_foreign" FOREIGN KEY("BabySitterId") REFERENCES "BabySitter"("BabySitterId");
+ALTER TABLE
+    "Request" ADD CONSTRAINT "request_babysitterid_foreign" FOREIGN KEY("BabySitterId") REFERENCES "BabySitter"("BabySitterId");
+ALTER TABLE
+    "Reviews" ADD CONSTRAINT "reviews_parentid_foreign" FOREIGN KEY("ParentId") REFERENCES "Parents"("ParentId");
+ALTER TABLE
+    "Request" ADD CONSTRAINT "request_parentid_foreign" FOREIGN KEY("ParentId") REFERENCES "Parents"("ParentId");
+ALTER TABLE
+    "User" ADD CONSTRAINT "user_locationid_foreign" FOREIGN KEY("LocationId") REFERENCES "Location"("LocationId");
+ALTER TABLE
+    "City" ADD CONSTRAINT "city_areaid_foreign" FOREIGN KEY("AreaId") REFERENCES "Area"("AreaId");
+ALTER TABLE
+    "Location" ADD CONSTRAINT "location_cityid_foreign" FOREIGN KEY("CityId") REFERENCES "City"("CityId");
+ALTER TABLE
+    "Request" ADD CONSTRAINT "request_requeststatusid_foreign" FOREIGN KEY("RequestStatusId") REFERENCES "RequestStatus"("RequestStatusId");
